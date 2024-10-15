d38471342f91bb91e3c68a51c9200b5736a39ff3
Lucas Silva <lucas.silva98@outlook.pt>
2024-10-15 17:11:39 +0100

    CRUD REST API created and implemented, with local database

--- src/main/java/dev/lucas/runnerz/run/RunController.java
+++ src/main/java/dev/lucas/runnerz/run/RunController.java
@@ -1,12 +1,57 @@
 package dev.lucas.runnerz.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+import java.util.Optional;
+
+@RestController // Marks this class as a Spring MVC controller
+@RequestMapping("api/runs") // Maps HTTP requests to /api/runs to this controller
 public class RunController {
-    @GetMapping("/hello")
-    String home() {
-        return "Hello, Runnerz!";
+
+    private final RunRepository runRepository;
+
+    // Constructor injection of RunRepository
+    public RunController(RunRepository runRepository){
+        this.runRepository = runRepository;
+    }
+
+    // FIND ALL
+    @GetMapping("") // Maps HTTP GET requests to /api/runs to this method
+    List<Run> findAll() {
+        return runRepository.findAll(); // Returns all runs
+    }
+
+    // FIND BY ID
+    @GetMapping("/{id}") // Maps HTTP GET requests to /api/runs/{id} to this method
+    Run findById(@PathVariable Integer id) { // Extracts the id from the URL
+        Optional<Run> run = runRepository.findById(id); // Finds the run by id
+        if (run.isEmpty()) {
+            throw new RunNotFoundException(); // Throws exception if run not found
+        }
+        return run.get(); // Returns the found run
+    }
+
+    // POST
+    @ResponseStatus(HttpStatus.CREATED) // Sets the response status to 201 Created
+    @PostMapping("") // Maps HTTP POST requests to /api/runs to this method
+    void create(@Valid @RequestBody Run run) { // Validates and extracts the request body
+        runRepository.create(run); // Creates a new run
+    }
+
+    // PUT
+    @ResponseStatus(HttpStatus.NO_CONTENT) // Sets the response status to 204 No Content
+    @PutMapping("/{id}") // Maps HTTP PUT requests to /api/runs/{id} to this method
+    void update(@RequestBody Run run, @PathVariable Integer id) { // Extracts the request body and id from the URL
+        runRepository.update(run, id); // Updates the run with the given id
+    }
+
+    // DELETE
+    @ResponseStatus(HttpStatus.NO_CONTENT) // Sets the response status to 204 No Content
+    @DeleteMapping("/{id}") // Maps HTTP DELETE requests to /api/runs/{id} to this method
+    void delete(@PathVariable Integer id) { // Extracts the id from the URL
+        runRepository.delete(id); // Deletes the run with the given id
     }
 }
