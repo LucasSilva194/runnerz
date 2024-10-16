b0904b29941b8cc528db9c63225e71332101d776
Lucas Silva <lucas.silva98@outlook.pt>
2024-10-16 14:36:54 +0100

    REST Clients
    Creation of HTTP clients

--- src/main/java/dev/lucas/runnerz/Application.java
+++ src/main/java/dev/lucas/runnerz/Application.java
@@ -1,9 +1,19 @@
 package dev.lucas.runnerz;
 
+import dev.lucas.runnerz.user.User;
+import dev.lucas.runnerz.user.UserHttpClient;
+import dev.lucas.runnerz.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -14,11 +24,19 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner runner(RunRepository runRepository) {
-//		return args -> {
-//			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1),5, Location.OUTDOOR);
-//            runRepository.create(run);
-//		};
-//	}
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
+		return args -> {
+			List<User> users = client.findAll();
+			System.out.println(users);
+		};
+
+	}
 }
